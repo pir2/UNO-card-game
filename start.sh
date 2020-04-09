391bc30e1461026cfdc7d0bd6eb95b1d479884cb
h6r <qyp@web.de>
2020-04-09 11:57:50 +0200

    fix invalid host header+2

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-screen -dmS uno-server node server/app.js
+screen -dmS uno-server node server/app.js --host 0.0.0.0 --disable-host-check
 cd client
 screen -dmS uno-client npm start --host 0.0.0.0  --disable-host-check
 echo uno-game started
