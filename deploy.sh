4a103db65138243112260344380f132dd6c2aa29
h6r <qyp@web.de>
2020-04-09 15:23:06 +0200

    fix script#3

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 git stash
 git pull
 cd client && npm install && npm run-script build
-rm -rf server/public
+rm -rf ../server/public
 mv build ../server/public
 cd .. && cd server && npm install
 screen -S uno -X quit
