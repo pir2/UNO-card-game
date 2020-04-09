e53ec66e66ab5c74854f3931fb5f6827519c625e
h6r <qyp@web.de>
2020-04-09 15:04:29 +0200

    fix prod build

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,7 @@
 git stash
 git pull
-cd client && npm install
+cd client && npm install && npm run-script build
 cd .. && cd server && npm install
-screen -S uno-server -X quit
-screen -S uno-client -X quit
+screen -S uno -X quit
 cd .. && chmod +x *.sh
 ./start.sh
--- start.sh
+++ start.sh
@@ -1,4 +1,2 @@
-screen -dmS uno-server node server/app.js --host 0.0.0.0 --disable-host-check
-cd client
-screen -dmS uno-client npm start --host 0.0.0.0  --disable-host-check
+screen -dmS uno node server/app.js --host 0.0.0.0 --disable-host-check
 echo uno-game started
